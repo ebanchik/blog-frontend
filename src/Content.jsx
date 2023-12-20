c411a9ca23254c98a2d3d6e9b970c79b8f331771
ebanchik <ebanchik@gmail.com>
2023-12-20 15:13:28 -0500

    add update feature

--- src/Content.jsx
+++ src/Content.jsx
@@ -25,17 +25,47 @@ const closeModal = () => {
   setIsPostsShowVisible(false)
 }
 
+const handleCreatePost = (params) => {
+  console.log('handling post create')
+  axios.post("http://localhost:3000/posts.json", params).then(response => {
+      // console.log(response.data)         
+      // posts + response.data
+      // posts.push(response.data)
+      setPosts([...posts, response.data])
+    })
+}
+
+const handleUpdatePost = (id, params) => {
+  console.log('handling update post')
+  axios.patch("http://localhost:3000/posts/" + id + ".json", params).then(response => {
+    console.log(response.data)
+    // posts array, add something to it
+    // posts array, modify something that already is in it 
+    // look through all the posts, find the right one, replace that with json from rails
+    setPosts(
+      posts.map(post => {
+        if (post.id === response.data.id) {
+          return response.data
+        } else {
+          return post
+        }
+      })
+    )
+    closeModal()
+  })
+}
+
 useEffect(handleIndexPosts, [])
 
   return (
     <div>
-      <PostsNew />
+      <PostsNew onCreatePost = {handleCreatePost}/>
       <br />
       <br />
       <button onClick={handleIndexPosts}>get the posts from the api</button>
       <PostsIndex posts={posts} onShowPosts={showModal}/>
       <Modal show={isPostsShowVisible} onClose={closeModal}>
-        <PostsShow post={currentPost} />
+        <PostsShow post={currentPost} onUpdatePost={handleUpdatePost}/>
       </Modal>
     </div>
   );
--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,10 +1,29 @@
+import axios from 'axios'
+
 export function PostsShow(props) {  
+  const updatePost = (event) => {
+    event.preventDefault()
+    const params = new FormData(event.target)
+    props.onUpdatePost(props.post.id, params)
+      console.log('updating post...')
+  }
   return (
     <div>
       <p>posts show</p>
+      <p><b>id: </b>{props.post.id}</p>
       <p><b>title: </b>{props.post.title}</p>
-      <p><b>ingredients: </b>{props.post.body}</p>
-      <img src={props.post.image_url} />
+      <p><b>body: </b>{props.post.body}</p>
+      <img src={props.post.image} />
+
+      <div>
+        <form onSubmit={updatePost}>
+          <p>title: <input type="text" name="title" defaultValue={props.post.title} /></p>
+          <p>body: <input type="text" name="body" defaultValue={props.post.body} /></p>
+          <p>image: <input type="text" name="image" defaultValue={props.post.image}/></p>
+          <button>Update Post</button>
+        </form>
+      </div>
+
     </div>
   );
 }
